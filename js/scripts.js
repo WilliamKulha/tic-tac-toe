66123ea63b67d65f38e061571b2ee38ee103f01a
William Kulha <kulha.william@gmail.com>
2018-10-18 10:47:43 -0400

    Capture user's requested play style as a variable.

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,17 @@
 
 
 
+
+
 //User Logic
 $(document).ready(function() {
   $('form#play_style_form').submit(function(event) {
     event.preventDefault();
+
     $('#player_select').slideUp();
     $('#game_board').slideDown();
+
+    let gameStyle = parseInt($('input:radio[name=play_style]:checked').val());
+    console.log(gameStyle);
   });
 });
